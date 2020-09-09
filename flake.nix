20d9fae274faaa3c70ba4de22146ca91c883d3d6
cidkidnix <cidkidnix@protonmail.com>
2020-09-08 20:49:11 -0500

    try some other  things

--- flake.nix
+++ flake.nix
@@ -1,17 +1,15 @@
 {
   description = "Home Manager for Nix";
 
-  outputs = { self, nixpkgs, config }: rec {
+  outputs = { self, nixpkgs }: rec {
     nixosModules.home-manager = import ./nixos nixpkgs;
-
-    darwinModules.home-manager = import ./nix-darwin nixpkgs;
 
     lib = {
       homeManagerConfiguration = { configuration, system, homeDirectory
         , username
         , pkgs ? builtins.getAttr system nixpkgs.outputs.legacyPackages
         , check ? true }@args:
-        import ./modules {
+        import ./modules nixpkgs {
           inherit pkgs check;
           configuration = { ... }: {
             imports = [ configuration ];
