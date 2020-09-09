04f11006cb05e96a2d04e2b806509ce51b2a4816
cidkidnix <cidkidnix@protonmail.com>
2020-09-08 20:45:02 -0500

    some other fixes

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Home Manager for Nix";
 
-  outputs = { self, nixpkgs }: rec {
+  outputs = { self, nixpkgs, config }: rec {
     nixosModules.home-manager = import ./nixos nixpkgs;
 
     darwinModules.home-manager = import ./nix-darwin nixpkgs;
